15cd1f03425f70502907f270118390830d42b120
Jowi <jowiaoun@hotmail.com>
2023-11-19 01:07:58 +0000

    1.1

--- src/logger.c
+++ src/logger.c
@@ -1,134 +1,155 @@
 #include "defs.h"
 
-/* 
-    Logs the hunter being created.
-    in: hunter - the hunter name to log
-    in: equipment - the hunter's equipment
+/*
+  Logs the hunter being created.
+  in: hunter - the hunter name to log
+  in: equipment - the hunter's equipment
 */
-void l_hunterInit(char* hunter, enum EvidenceType equipment) {
-    if (!LOGGING) return;
-    char ev_str[MAX_STR];
-    evidenceToString(equipment, ev_str);
-    printf("[HUNTER INIT] [%s] is a [%s] hunter\n", hunter, ev_str);    
+void l_hunterInit(char *hunter, enum EvidenceType equipment)
+{
+  if (!LOGGING)
+    return;
+  char ev_str[MAX_STR];
+  evidenceToString(equipment, ev_str);
+  printf("[HUNTER INIT] [%s] is a [%s] hunter\n", hunter, ev_str);
 }
 
 /*
-    Logs the hunter moving into a new room.
-    in: hunter - the hunter name to log
-    in: room - the room name to log
+  Logs the hunter moving into a new room.
+  in: hunter - the hunter name to log
+  in: room - the room name to log
 */
-void l_hunterMove(char* hunter, char* room) {
-    if (!LOGGING) return;
-    printf("[HUNTER MOVE] [%s] has moved into [%s]\n", hunter, room);
+void l_hunterMove(char *hunter, char *room)
+{
+  if (!LOGGING)
+    return;
+  printf("[HUNTER MOVE] [%s] has moved into [%s]\n", hunter, room);
 }
 
 /*
-    Logs the hunter exiting the house.
-    in: hunter - the hunter name to log
-    in: reason - the reason for exiting, either LOG_FEAR, LOG_BORED, or LOG_EVIDENCE
+  Logs the hunter exiting the house.
+  in: hunter - the hunter name to log
+  in: reason - the reason for exiting, either LOG_FEAR, LOG_BORED, or LOG_EVIDENCE
 */
-void l_hunterExit(char* hunter, enum LoggerDetails reason) {
-    if (!LOGGING) return;
-    printf("[HUNTER EXIT] [%s] exited because ", hunter);
-    switch (reason) {
-        case LOG_FEAR:
-            printf("[FEAR]\n");
-            break;
-        case LOG_BORED:
-            printf("[BORED]\n");
-            break;
-        case LOG_EVIDENCE:
-            printf("[EVIDENCE]\n");
-            break;
-        default:
-            printf("[UNKNOWN]\n");
-    }
+void l_hunterExit(char *hunter, enum LoggerDetails reason)
+{
+  if (!LOGGING)
+    return;
+  printf("[HUNTER EXIT] [%s] exited because ", hunter);
+  switch (reason)
+  {
+  case LOG_FEAR:
+    printf("[FEAR]\n");
+    break;
+  case LOG_BORED:
+    printf("[BORED]\n");
+    break;
+  case LOG_EVIDENCE:
+    printf("[EVIDENCE]\n");
+    break;
+  default:
+    printf("[UNKNOWN]\n");
+  }
 }
 
 /*
-    Logs the hunter reviewing evidence.
-    in: hunter - the hunter name to log
-    in: result - the result of the review, either LOG_SUFFICIENT or LOG_INSUFFICIENT
+  Logs the hunter reviewing evidence.
+  in: hunter - the hunter name to log
+  in: result - the result of the review, either LOG_SUFFICIENT or LOG_INSUFFICIENT
 */
-void l_hunterReview(char* hunter, enum LoggerDetails result) {
-    if (!LOGGING) return;
-    printf("[HUNTER REVIEW] [%s] reviewed evidence and found ", hunter);
-    switch (result) {
-        case LOG_SUFFICIENT:
-            printf("[SUFFICIENT]\n");
-            break;
-        case LOG_INSUFFICIENT:
-            printf("[INSUFFICIENT]\n");
-            break;
-        default:
-            printf("[UNKNOWN]\n");
-    }
+void l_hunterReview(char *hunter, enum LoggerDetails result)
+{
+  if (!LOGGING)
+    return;
+  printf("[HUNTER REVIEW] [%s] reviewed evidence and found ", hunter);
+  switch (result)
+  {
+  case LOG_SUFFICIENT:
+    printf("[SUFFICIENT]\n");
+    break;
+  case LOG_INSUFFICIENT:
+    printf("[INSUFFICIENT]\n");
+    break;
+  default:
+    printf("[UNKNOWN]\n");
+  }
 }
 
 /*
-    Logs the hunter collecting evidence.
-    in: hunter - the hunter name to log
-    in: evidence - the evidence type to log
-    in: room - the room name to log
+  Logs the hunter collecting evidence.
+  in: hunter - the hunter name to log
+  in: evidence - the evidence type to log
+  in: room - the room name to log
 */
-void l_hunterCollect(char* hunter, enum EvidenceType evidence, char* room) {
-    if (!LOGGING) return;
-    char ev_str[MAX_STR];
-    evidenceToString(evidence, ev_str);
-    printf("[HUNTER EVIDENCE] [%s] found [%s] in [%s] and [COLLECTED]\n", hunter, ev_str, room);
+void l_hunterCollect(char *hunter, enum EvidenceType evidence, char *room)
+{
+  if (!LOGGING)
+    return;
+  char ev_str[MAX_STR];
+  evidenceToString(evidence, ev_str);
+  printf("[HUNTER EVIDENCE] [%s] found [%s] in [%s] and [COLLECTED]\n", hunter, ev_str, room);
 }
 
 /*
-    Logs the ghost moving into a new room.
-    in: room - the room name to log
+  Logs the ghost moving into a new room.
+  in: room - the room name to log
 */
-void l_ghostMove(char* room) {
-    if (!LOGGING) return;
-    printf("[GHOST MOVE] Ghost has moved into [%s]\n", room);
+void l_ghostMove(char *room)
+{
+  if (!LOGGING)
+    return;
+  printf("[GHOST MOVE] Ghost has moved into [%s]\n", room);
 }
 
 /*
-    Logs the ghost exiting the house.
-    in: reason - the reason for exiting, either LOG_FEAR, LOG_BORED, or LOG_EVIDENCE
+  Logs the ghost exiting the house.
+  in: reason - the reason for exiting, either LOG_FEAR, LOG_BORED, or LOG_EVIDENCE
 */
-void l_ghostExit(enum LoggerDetails reason) {
-    if (!LOGGING) return;
-    printf("[GHOST EXIT] Exited because ");
-    switch (reason) {
-        case LOG_FEAR:
-            printf("[FEAR]\n");
-            break;
-        case LOG_BORED:
-            printf("[BORED]\n");
-            break;
-        case LOG_EVIDENCE:
-            printf("[EVIDENCE]\n");
-            break;
-        default:
-            printf("[UNKNOWN]\n");
-    }
+void l_ghostExit(enum LoggerDetails reason)
+{
+  if (!LOGGING)
+    return;
+  printf("[GHOST EXIT] Exited because ");
+  switch (reason)
+  {
+  case LOG_FEAR:
+    printf("[FEAR]\n");
+    break;
+  case LOG_BORED:
+    printf("[BORED]\n");
+    break;
+  case LOG_EVIDENCE:
+    printf("[EVIDENCE]\n");
+    break;
+  default:
+    printf("[UNKNOWN]\n");
+  }
 }
 
 /*
-    Logs the ghost leaving evidence in a room.
-    in: evidence - the evidence type to log
-    in: room - the room name to log
+  Logs the ghost leaving evidence in a room.
+  in: evidence - the evidence type to log
+  in: room - the room name to log
 */
-void l_ghostEvidence(enum EvidenceType evidence, char* room) {
-    if (!LOGGING) return;
-    char ev_str[MAX_STR];
-    evidenceToString(evidence, ev_str);
-    printf("[GHOST EVIDENCE] Ghost left [%s] in [%s]\n", ev_str, room);
+void l_ghostEvidence(enum EvidenceType evidence, char *room)
+{
+  if (!LOGGING)
+    return;
+  char ev_str[MAX_STR];
+  evidenceToString(evidence, ev_str);
+  printf("[GHOST EVIDENCE] Ghost left [%s] in [%s]\n", ev_str, room);
 }
 
 /*
-    Logs the ghost being created.
-    in: ghost - the ghost type to log
-    in: room - the room name that the ghost is starting in
+  Logs the ghost being created.
+  in: ghost - the ghost type to log
+  in: room - the room name that the ghost is starting in
 */
-void l_ghostInit(enum GhostClass ghost, char* room) {
-    if (!LOGGING) return;
-    char ghost_str[MAX_STR];
-    ghostToString(ghost, ghost_str);
-    printf("[GHOST INIT] Ghost is a [%s] in room [%s]\n", ghost_str, room);
+void l_ghostInit(enum GhostClass ghost, char *room)
+{
+  if (!LOGGING)
+    return;
+  char ghost_str[MAX_STR];
+  ghostToString(ghost, ghost_str);
+  printf("[GHOST INIT] Ghost is a [%s] in room [%s]\n", ghost_str, room);
 }
--- src/main.c
+++ src/main.c
@@ -1,17 +1,21 @@
 #include "defs.h"
 
 int main() {
-    // Initialize the random number generator
-    srand(time(NULL));
+  // Initialize the random number generator
+  srand(time(NULL));
 
-    // Create the house: You may change this, but it's here for demonstration purposes
-    // Note: This code will not compile until you have implemented the house functions and structures
-    HouseType house;
-    initHouse(&house);
-    populateRooms(&house);
+  // Create the house: You may change this, but it's here for demonstration purposes
+  // Note: This code will not compile until you have implemented the house functions and structures
+  HouseType house;
+  initHouse(&house);
+  populateRooms(&house);
 
-    char[MAX_STR] hunterName;
-    
+  char hunterName[MAX_STR];
+  for (int i = 0; i < 4; i++) {
+    scanf("%s", hunterName);
+    while (getchar() != '\n');
+  }
+  
 
-    return 0;
+  return 0;
 }
