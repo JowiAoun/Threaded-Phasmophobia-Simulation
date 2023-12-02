3db782361765c491df9f1f166bb2c60ae7d13b08
Param Desai <93168030+ParamDesai111@users.noreply.github.com>
2023-12-02 20:26:00 +0000

    Hunter Thread Attempt

--- src/hunter.c
+++ src/hunter.c
@@ -12,11 +12,40 @@ void initHunter(char* name, enum EvidenceType equipment,
   (*hunter)->fear = 0;
   (*hunter)->room = 0;
 }
+void moveHunterRooms(HunterType* hunter){
+  int roomIndex = randInt(0,hunter->room->roomList->size-1);
+  RoomNodeType* currentRoom = hunter->room->roomList->head;
+  for(int i = 0; i <roomIndex; i++){
+    currentRoom = currentRoom->next;
+  }
+  for(int i = 0; i < NUM_HUNTERS; i++){
+  currentRoom->data->hunter[i] = NULL;
+  currentRoom->data->hunters
+
+  }
+  currentRoom->data->hunter = NULL;
+  hunter->room = currentRoom->data;
+  currentRoom->data->hunter = hunter;
+  l_ghostMove(currentRoom->data->name);
+
+
+}
 
 void* hunter_thread(void* arg) {
   HunterType* hunter = (HunterType*) arg;
 
   while (1) {
+    // 3.1
+    if(hunter->room->ghost != NULL){
+      hunter->fear += 1;
+      hunter->boredom = 0;
+    }
+    else{
+      hunter->boredom += 1;
+    }
+    
+
+
     usleep(HUNTER_WAIT);
   }
 
